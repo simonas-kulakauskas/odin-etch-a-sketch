d8c594b209fb626d5aa691404a2d85eafe08e804
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-24 01:32:32 +0100

    Changed the hardcoded amount of squares per side to a variable that I can later change or get user to pick

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 // Select container div that we will be adding all the other divs to.
 const containerDiv = document.querySelector("#container");
+let squaresPerSide = 16;
 
-// Creates 16 div boxes as rows
-for (let i = 0; i < 16; i++) {
+// Creates chosen amount of div boxes as rows & cols
+for (let i = 0; i < squaresPerSide; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.className = "row";
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < squaresPerSide; j++) {
         const colDiv = document.createElement("div");
         colDiv.className = "col";
         rowDiv.appendChild(colDiv);
