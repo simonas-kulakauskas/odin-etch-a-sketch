53f3b929d41beb37f006e421e801f588aa907809
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-24 01:03:54 +0100

    Created a 16x16 grid in javascript. Used flexbox in css to lay them out in a box shape

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 // Select container div that we will be adding all the other divs to.
 const containerDiv = document.querySelector("#container");
+
+// Creates 16 div boxes as rows
+for (let i = 0; i < 16; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "row";
+    for (let j = 0; j < 16; j++) {
+        const colDiv = document.createElement("div");
+        colDiv.className = "col";
+        rowDiv.appendChild(colDiv);
+    }
+    containerDiv.appendChild(rowDiv);
+}
+
