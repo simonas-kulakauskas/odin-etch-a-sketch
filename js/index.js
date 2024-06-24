6fe8a488f60c4e16fda85e27944ae6295eff803f
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-24 01:41:49 +0100

    Added a todo list within the .js to track progress and my targets.

--- js/index.js
+++ js/index.js
@@ -27,3 +27,15 @@ for (let i = 0; i < colDivs.length; i++) {
     colDivs[i].addEventListener("click", changeDivColor);
 }
 
+/* // TODO: LIST
+ *  1.❎ Get boxes to flex and resize properly, no matter how many boxes are used.
+    2.❎ Changed from a 'click' event to a 'mouseover' (hover) event to draw whenever mouse is above it.
+    3.❎ Add a button at the top of the screen to prompt the user with how many squares they want per side.
+        a. Once entered, should remove the old grid and add new with the same total space as previously.
+        (TIP) : User input limit should be 100 as larger number sequences may cause delays and freezing.
+
+    ! Challenge 1: ❎
+        - Randomise RGB values for each interaction to change the color randomly.
+    ! Challenge 2: ❎
+        - Implement a progressive darkening effect where it darkens each square by 10% each time it's pressed.
+*/
